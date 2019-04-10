1fb8a7d5fce4ecb269c773ff1afc7f50008d0a8e
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2019-04-11 00:01:21 +0500

    Initial commit by @shaizei/cli

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: '@shaizei/eslint-config/javascriptReact',
+};
---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React, { StrictMode } from 'react';
+import { render } from 'react-dom';
+import { setConfig } from 'react-hot-loader';
+import App from 'components/App';
+
+setConfig({
+  pureRender: true,
+  ignoreSFC: true,
+});
+
+const root = document.getElementById('root');
+
+if (!root) throw new Error('Unable to mount your app.');
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  root,
+);
